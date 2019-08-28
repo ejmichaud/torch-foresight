8ce0a2c6021106625d4b5566830404fa67c4de3a
Eric Michaud <eric.michaud99@gmail.com>
2019-08-28 09:54:40 -0700

    set master_doc to index (default was content

--- docs/conf.py
+++ docs/conf.py
@@ -71,6 +71,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
 
 # -- Options for LaTeX output ---------------------------------------------
 
